689a73eade81147abeb0a6bf047b887426de154c
Dicky April Zahdi <bangkadev@gmail.com>
2025-05-12 16:44:58 +0700

    Model Aplikasi Pos

--- app/Models/WarehouseProduct.php
+++ app/Models/WarehouseProduct.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WarehouseProduct extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['product_id', 'warehouse_id', 'stock'];
+
+    public function warehouse()
+    {
+        return $this->belongsTo(Warehouse::class);
+    }  
+    
+    public function product () {
+        return $this->belongsTo(Product::class);
+    }
 }
